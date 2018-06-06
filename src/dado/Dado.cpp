07a51249e42e4e7186dad9c789fb9ddf16e0ddc0
FrancescoCerio <francescocerio98@gmail.com>
2018-06-06 18:00:42 +0200

    classe dado aggiunta

---
+++ src/dado/Dado.cpp
@@ -0,0 +1,112 @@
+//
+//  Dado.cpp
+//  Gioco dell'oca pazza
+//
+//  Created by Francesco Cerio on 16/05/18.
+//  Copyright © 2018 Francesco Cerio. All rights reserved.
+//
+
+#include "Dado.hpp"
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+Dado::Dado(){}
+
+int Dado::lanciaDado(){
+
+    srand((unsigned)time(NULL));
+    
+    return this->d = rand() % 6 + 1;
+    
+}
+
+
+void Dado::mostraRisultato(){
+    lanciaDado();
+    disegnaDado();
+    cout << "Hai totalizzato " << this->d << "!" << endl;
+}
+
+
+void Dado::disegnaDado(){
+    
+    cout << endl;
+    cout << "   ______     " << endl;
+    cout << "  /     /\\   " << endl;
+    cout << " /  '  /  \\  " << endl;
+    cout << "/_____/. . \\ " << endl;
+    cout << "\\ . . \\    /" << endl;
+    cout << " \\ . . \\  / " << endl;
+    cout << "  \\_____\\/ " << endl << endl;
+    
+    usleep(500000);
+    
+    switch (this->d) {
+        case 1:
+            
+            cout << " _______ " << endl;
+            cout << "|       |" << endl;
+            cout << "|   .   |" << endl;
+            cout << "|       |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+        case 2:
+            
+            cout << " _______ " << endl;
+            cout << "| .     |" << endl;
+            cout << "|       |" << endl;
+            cout << "|     . |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+            
+        case 3:
+            
+            cout << " _______ " << endl;
+            cout << "| .     |" << endl;
+            cout << "|   .   |" << endl;
+            cout << "|     . |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+            
+        case 4:
+            
+            cout << " _______ " << endl;
+            cout << "| .   . |" << endl;
+            cout << "|       |" << endl;
+            cout << "| .   . |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+            
+        case 5:
+            
+            cout << " _______ " << endl;
+            cout << "| .   . |" << endl;
+            cout << "|   .   |" << endl;
+            cout << "| .   . |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+            
+        case 6:
+            
+            cout << " _______ " << endl;
+            cout << "| . . . |" << endl;
+            cout << "| . . . |" << endl;
+            cout << "| . . . |" << endl;
+            cout << " ------- " << endl;
+            
+            break;
+            
+        default:
+            cout << " ";
+            break;
+    }
+}
